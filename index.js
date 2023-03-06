bea12cd213acf326fdcbbc7c405d08a8d333c132
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-03-06 15:42:26 +0800

    Multiple updates

---
+++ index.js
@@ -0,0 +1,19 @@
+function Spa() {
+    return (
+      <HashRouter>
+        <NavBar/>
+        <UserContext.Provider value={{users:[{name:'dan',email:'dan@liljestig.com',password:'T0p$3cret',balance:100}]}}>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/login/" component={Login} />
+            <Route path="/deposit/" component={Deposit} />
+            <Route path="/withdraw/" component={Withdraw} />
+            <Route path="/balance/" component={Balance} />
+            <Route path="/alldata/" component={AllData} />
+        </UserContext.Provider>      
+      </HashRouter>
+    );
+}
+  
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Spa/>);
