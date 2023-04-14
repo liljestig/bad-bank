2044e73f3caff6a97cd4adb459b90efa89ec7dfc
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-04-14 19:20:47 +0800

    Multiple updates
    alldata.js, alldata.js.bak

--- alldata.js
+++ alldata.js
@@ -1,8 +1,33 @@
 function AllData() {
     const ctx = React.useContext(UserContext);
     return (
-        <h1>All Data<br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card
+            style={{ width: '35rem' }}
+            bgcolor="light"
+            txtcolor="dark"
+            header="All Data"
+            title="All the bank's account data is listed below"
+            text="Be careful to use any data with an honest intention."
+            body={(<table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                        <th>Balance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ctx.users.map((user, index) => (
+                        <tr key={index}>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.password}</td>
+                            <td>{user.balance}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>)}
+        />
     );
 }
