b07c91da1a91b6437ea7ef817c849bc94c097cea
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-04-14 18:30:12 +0800

    Update home.js

--- home.js
+++ home.js
@@ -2,12 +2,12 @@ function Home() {
     const ctx = React.useContext(UserContext);
     return (
         <Card
-        bgcolor="light"
-        txtcolor="dark"
-        header="BadBank Landing Module"
-        title="Welcome to the bank"
-        text="You can move around using the navigation bar."
-        body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+            bgcolor="light"
+            txtcolor="dark"
+            header="BadBank Landing Module"
+            title="Welcome to the bank"
+            text="You can move around using the navigation bar."
+            body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
         />
     );
 }
