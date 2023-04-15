87965ae3884f1204be68cf4ecea667a55be75d1d
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-04-15 18:35:34 +0800

    Multiple updates
    Update login.js
    Update login.js.bak

--- login.js
+++ login.js
@@ -1,8 +1,43 @@
 function Login() {
+    const [show, setShow]         = React.useState(true);
+    const [status, setStatus]     = React.useState('');
+    const [email, setEmail]       = React.useState('');
+    const [password, setPassword] = React.useState('');
     const ctx = React.useContext(UserContext);
+
+    function validate(field, label){
+        if (!field) {
+            setStatus('Error: ' + label);
+            setTimeout(() => setStatus(''), 3000);
+            return false;
+        }
+        return true;
+    }
+
+    function handleLogin(){
+        console.log(email,password);
+        const user = ctx.users.find((user) => user.email === email && user.password === password);
+        setShow(false);
+    }
+
     return (
-        <h1>Login<br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card
+        bgcolor="light"
+        txtcolor="dark"
+        header="Login"
+        body={show ? (  
+            <>
+            Email address<br/>
+            <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+            Password<br/>
+            <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+            <button type="submit" className="btn btn-secondary" onClick={handleLogin}>Login</button>
+            </>
+            ):(
+            <>
+            <h5>Success</h5>
+            </>
+            )}
+        />
     );
 }
