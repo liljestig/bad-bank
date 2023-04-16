4321474a583685e7e6416d68b78f42221055d4fb
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-04-16 14:01:08 +0800

    Update context.js

--- context.js
+++ context.js
@@ -3,6 +3,8 @@ const Link        = ReactRouterDOM.Link;
 const HashRouter  = ReactRouterDOM.HashRouter;
 const UserContext = React.createContext(null);
 
+const Table       = React.Table;
+
 function Card(props){    
     function classes(){
         const bg  = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
